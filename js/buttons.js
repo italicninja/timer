32b3f55a4da21b760dd5819246eaa00fe89c7159
Ian <ian.b.crouch@gmail.com>
2023-08-30 00:59:51 +0000

    Updating for dark mode support

--- js/buttons.js
+++ js/buttons.js
@@ -1,14 +1,26 @@
-const darklighttoggle = document.getElementById("darklighttoggle")
+// Wait for the DOM content to load before working with elements
+document.addEventListener("DOMContentLoaded", function() {
+  const darklighttoggle = document.getElementById("darklighttoggle");
 
-darklighttoggle.addEventListener("change", () => {
-  document.body.classList.toggle("dark", darklighttoggle.checked);
-})
+  darklighttoggle.addEventListener("change", () => {
+    // Use the checked property to determine the toggle state
+    if (darklighttoggle.checked) {
+      document.body.classList.add("dark");
+      document.box.classList.add("dark");
+      document.blank.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+      document.box.classList.remove("dark");
+      document.blank.classList.remove("dark");
+    }
+  });
+});
 
-function hideGuild() {
+/* function hideGuild() {
   var x = document.getElementById("guildTable");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
-}
+} */
