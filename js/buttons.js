a338ebd8cfb76c3022833be5af96848d41375caf
Ian <ian.b.crouch@gmail.com>
2023-09-02 00:46:29 +0000

    preliminary dark mode

--- js/buttons.js
+++ js/buttons.js
@@ -6,21 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // Use the checked property to determine the toggle state
     if (darklighttoggle.checked) {
       document.body.classList.add("dark");
-      document.box.classList.add("dark");
-      document.blank.classList.add("dark");
     } else {
       document.body.classList.remove("dark");
-      document.box.classList.remove("dark");
-      document.blank.classList.remove("dark");
     }
   });
 });
-
-/* function hideGuild() {
-  var x = document.getElementById("guildTable");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-} */
